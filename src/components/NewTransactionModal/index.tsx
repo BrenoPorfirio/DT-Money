e352a27db9c601466d58b9886dfd78bda7d827f9
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-11-01 14:10:14 -0300

    create form and button

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,13 +1,24 @@
 import * as Dialog from "@radix-ui/react-dialog";
+import { Content, Overlay } from "./styles";
 
 export function NewTransactionModal() {
     return (
         <Dialog.Portal>
-            <Dialog.Overlay />
-            <Dialog.Content>
+            <Overlay />
+            <Content>
                 <Dialog.Title>Nova transação</Dialog.Title>
+
+                <form action="">
+                    <input type="text" placeholder="Descrição" required />
+                    <input type="number" placeholder="Preço" required />
+                    <input type="text" placeholder="Categoria" required />
+                    <button type="submit">
+                        Cadastrar
+                    </button>
+                </form>
+
                 <Dialog.Close />
-            </Dialog.Content>
+            </Content>
         </Dialog.Portal>
     )
 }

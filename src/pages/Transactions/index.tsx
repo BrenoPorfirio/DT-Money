aa2ce4f18ec74b0b63d321739ecd09b52b56956d
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-11-05 18:23:15 -0300

    remove request api from transaction

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -5,19 +5,6 @@ import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
-    const [transactions, setTransactions] = useState<Transaction[]>([])
-
-    async function loadTransactions() {
-            const response = await fetch('http://localhost:3333/transactions')
-            const data = await response.json();
-
-            setTransactions(data);
-        }
-
-    useEffect(() => {
-        loadTransactions();
-    }, [])
-
     return (
         <div>
             <Header />

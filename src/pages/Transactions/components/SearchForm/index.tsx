8893146918eab212f90a8fe7834ddf7a9a0c1737
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-11-11 19:14:22 -0300

    eslint config

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,45 +1,44 @@
-import { MagnifyingGlass } from "phosphor-react";
-import { SearchFormContainer } from "./styles";
-import { useForm } from "react-hook-form";
-import * as z from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useContext } from "react";
-import { TransactionsContext } from "../../../../contexts/TransactionsContext";
-
+import { MagnifyingGlass } from 'phosphor-react'
+import { SearchFormContainer } from './styles'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useContext } from 'react'
+import { TransactionsContext } from '../../../../contexts/TransactionsContext'
 
 const searchFormSchema = z.object({
-    query: z.string(),
+  query: z.string(),
 })
 
-type SearchFormInput = z.infer<typeof searchFormSchema>;
+type SearchFormInput = z.infer<typeof searchFormSchema>
 
 export function SearchForm() {
-    const { fetchTransactions } = useContext(TransactionsContext)
-    
-    const {
-        register, 
-        handleSubmit,
-        formState: { isSubmitting }
-        } = useForm<SearchFormInput>({
-        resolver: zodResolver(searchFormSchema)
-    })
-    
-    async function handleSearchTransactions(data: SearchFormInput) {
-        await fetchTransactions(data.query)
-    }
+  const { fetchTransactions } = useContext(TransactionsContext)
 
-    return (
-        <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
-            <input
-            type="text"
-            placeholder="Busque por transações"
-            {...register('query')}
-            />
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SearchFormInput>({
+    resolver: zodResolver(searchFormSchema),
+  })
 
-            <button type="submit" disabled={isSubmitting}>
-                <MagnifyingGlass size={20}/>
-                Buscar
-            </button>
-        </SearchFormContainer>
-    )
+  async function handleSearchTransactions(data: SearchFormInput) {
+    await fetchTransactions(data.query)
+  }
+
+  return (
+    <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+      <input
+        type="text"
+        placeholder="Busque por transações"
+        {...register('query')}
+      />
+
+      <button type="submit" disabled={isSubmitting}>
+        <MagnifyingGlass size={20} />
+        Buscar
+      </button>
+    </SearchFormContainer>
+  )
 }
